18c50e1dcd8b9888973a2513b01326376901def3
marcosmpar <marcosmpar@gmail.com>
2020-09-23 21:40:34 -0400

    feat: eslint config && prettier config

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,13 @@
 import express from 'express';
 
-import Routes from './routes';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.use(Routes);
+app.use(routes);
 
 app.listen(3333, () => {
-    console.log('Server started on port 3333!');
+  console.log('Server started on port 3333!');
 });
-
-
